40c175d14801778b4a30be1f55f10d0f5efa6731
Tema Ovchinnikov <flatsplit18@gmail.com>
2021-10-30 07:08:31 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './posts/post/post.component';
+import { ButtonComponent } from './posts/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
